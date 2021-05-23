ff7d01f63eafd1b701406ec6a7795aafcbe73572
Lucas "Massive" Zanisk <lucaszanisk@gmail.com>
2021-05-23 16:56:00 -0300

    Update App.js
    Imprimido a lista  de livros na tela

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ class App extends Component {
   };
 
   async componentDidMount() {
-    const response = await api.get('', { params: { q: 'harry' } });
-    console.log(response.data);
-
-    this.setState({ livros: response.data });
+    const response = await api.get('', { params: { q: 'harry' } }); //requisição de busca palavra 'harry'
+    //console.log(response.data.items)
+    this.setState({ livros: response.data.items });
   }
 
   render() {
-    const { livros } = this.state;
-
     return (
       <div>
         <h1>Listar os filmes</h1>
+        {console.log(this.state.livros)}
+        {this.state.livros.map(livro =>(
+          <li key= {livro.id}>
+            <p> {livro.volumeInfo.title} </p>
+
+          </li>
+        ))}
       </div>
     );
   }
